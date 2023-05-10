b3b67cb8b812a6a8b7f674974adfd8f30f116209
Riccardo Papini <rickypapini@gmail.com>
2023-05-10 23:21:20 +0200

    Fix circles

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -19,23 +19,23 @@ const Hero = (props: Props) => {
   return (
     <div className="h-screen flex flex-col  items-center justify-center text-center overflow-hidden">
       <BackgroundCirecle />
-      <img
+      {/* <img
         src="/temp.jpg"
         alt="hero-image"
         className="relative rounded-full h-32 w-32 mx-auto object-cover"
-      />
-      {/* <Image
+      /> */}
+      <Image
         className="relative rounded-full h-32 w-32 mx-auto object-cover"
         src="/temp.jpg"
         width={100}
         height={100}
         alt="hero-image"
-      /> */}
+      />
       <div className="z-20">
         <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
           Front-End Developer
         </h2>
-        <h1 className="text-4xl lg:text-5xl font-semibold px-10">
+        <h1 className="text-2xl lg:text-5xl font-semibold px-10">
           <span className="mr-3">{text}</span>
           <Cursor cursorColor="#F7ABOA" />
         </h1>
